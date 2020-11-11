d6310504609506bab5e9b9e773ebade7bd0b441a
joapim <48946611+joapim@users.noreply.github.com>
2020-11-11 17:59:39 +0100

    Add files via upload

---
+++ 1.klasse/IT grunnkurs/Øvinger/Øving 1/Kalkulasjoner.py
@@ -0,0 +1,7 @@
+print('Hello world!')
+forste_tall = int(input('Skriv et tall: '))
+andre_tall = int(input('Skriv enda et tall: '))
+print('Summen av tallene er: ',forste_tall + andre_tall)
+print('Moduloen til de to tallene er: ',forste_tall % andre_tall)
+#Dette er en kommentar
+                  
